345cc406ba29aac15f2b37ca311d61a3db84f403
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2023-10-11 23:17:26 -0300

    implementing routes

---
+++ src/pages/Contact/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export function Contact() {
+  return (
+    <S.Container>
+      <S.Title>Fale com o organizador</S.Title>
+    </S.Container>
+  )
+}
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Home } from '../pages/Home'
+import { Events } from '../pages/Events'
+import { Contact } from '../pages/Contact'
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="events">
+      <Screen name="home" component={Home} />
+      <Screen name="events" component={Events} />
+      <Screen name="contact" component={Contact} />
+    </Navigator>
+  )
+}
